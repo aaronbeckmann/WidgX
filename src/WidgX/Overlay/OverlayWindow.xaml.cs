b32769f23db3e0116475986f3ad746032ed6e1d8
Aaron Beckmann <aaron.beckmann26@gmail.com>
2026-06-27 13:31:28 +0200

    Add OverlayWindow with click-through WM_NCHITTEST hook and desktop-layer reparenting

---
+++ src/WidgX/Overlay/OverlayWindow.xaml.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Interop;
+using WidgX.Native;
+
+namespace WidgX.Overlay;
+
+public partial class OverlayWindow : Window
+{
+    private List<Rect> _widgetBounds = new();
+    private HwndSource? _hwndSource;
+
+    public OverlayWindow(Rect screenBoundsDip)
+    {
+        InitializeComponent();
+
+        Left = screenBoundsDip.X;
+        Top = screenBoundsDip.Y;
+        Width = screenBoundsDip.Width;
+        Height = screenBoundsDip.Height;
+
+        SourceInitialized += OnSourceInitialized;
+    }
+
+    public void SetWidgetBounds(IEnumerable<Rect> bounds)
+    {
+        _widgetBounds = new List<Rect>(bounds);
+    }
+
+    private void OnSourceInitialized(object? sender, EventArgs e)
+    {
+        var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+        _hwndSource = HwndSource.FromHwnd(hwnd);
+        _hwndSource?.AddHook(WndProc);
+
+        DesktopLayerHelper.SendToDesktopLayer(hwnd);
+    }
+
+    private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+    {
+        if (msg == NativeMethods.WM_NCHITTEST)
+        {
+            var screenX = unchecked((short)((long)lParam & 0xFFFF));
+            var screenY = unchecked((short)(((long)lParam >> 16) & 0xFFFF));
+            var pointInWindow = PointFromScreen(new Point(screenX, screenY));
+
+            var isOverWidget = ClickThroughHitTester.HitTest(pointInWindow, _widgetBounds);
+
+            handled = true;
+            return isOverWidget ? (IntPtr)NativeMethods.HTCLIENT : (IntPtr)NativeMethods.HTTRANSPARENT;
+        }
+
+        return IntPtr.Zero;
+    }
+}
